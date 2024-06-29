8281896ab6d83a1f14c0f02efe0ab89789662ace
Alex Mikulich <4205353@gmail.com>
2024-06-30 00:02:33 +0300

    creat a first level

---
+++ Enemy.gd
@@ -0,0 +1,18 @@
+extends PathFollow2D
+
+var speed = 100 # 100
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	set_progress(get_progress() + speed * delta)
+
+	if get_progress_ratio() == 1:
+		defeat()
+
+func defeat():
+	queue_free()
---
+++ FAMCS.TD_design-main/README.md
@@ -0,0 +1,2 @@
+# FAMCS.TD_design
+FAMCS.TD is a tower defence game from FAMCS students
