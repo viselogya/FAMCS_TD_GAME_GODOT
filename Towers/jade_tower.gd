5eb7b1ab533cd4d7feba6f371f768d9fc73cc2b2
Alex Mikulich <4205353@gmail.com>
2024-07-05 05:59:57 +0300

    работающая версия

---
+++ Towers/jade_tower.gd
@@ -0,0 +1,13 @@
+extends Tower
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	super._ready()
+
+
+func get_bullet_scene_path():
+	return "res://Towers/wooden_bullet.tscn"
+
+func get_type_of_tower():
+	return "jade_tower"
