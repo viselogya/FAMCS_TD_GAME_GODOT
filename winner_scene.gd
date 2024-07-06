d3565f43f135b86963480291761dceae917cc9d8
Alex Mikulich <4205353@gmail.com>
2024-07-07 01:34:45 +0300

    исправил пробелему с оторбражением cell и проблему с музыкой во время победы

--- winner_scene.gd
+++ winner_scene.gd
@@ -2,6 +2,9 @@ extends Node2D
 
 @onready var music_winner = $music_winner
 
-func _on_back_to_menu_pressed():
+func _ready():
 	music_winner.play()
+
+func _on_back_to_menu_pressed():
+	music_winner.stop()
 	get_tree().change_scene_to_file("res://menu.tscn")
